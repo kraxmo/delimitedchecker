5bffad7ff1d415522b901030fafcc89a02ffe5e2
Jim Kraxberger <jkraxberger@schoolsfirstfcu.org>
2026-01-13 07:53:35 -0800

    refactored output to logging and mocked file access in unittest

--- delimitedfilechecker.py
+++ delimitedfilechecker.py
@@ -1,166 +1,180 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@purpose: This script checks for embedded pipe characters within pipe-delimited file 
+@purpose: This script checks for embedded pipe characters within pipe-delimited file
           where header count != detail count and writes bad records to output file
 @author : Jim Kraxberger
 @created: 2021-11-16
-@updated: 2024-11-01
-@command: (UNIX)    python delimitedfilechecker.py --delimiter '|' --filename 'filename' -v
-          (Windows) python delimitedfilechecker.py --delimiter "|" --filename "filename" -v
+@updated: 2026-01-12
+@command: (UNIX)    python delimitedfilechecker.py ',' 'filename'
+          (Windows) python delimitedfilechecker.py "," "filename"
 """
 
 import argparse as ap
 import csv
 from datetime import datetime
+import logging
+import sys
 from typing import Iterable
 
-HELP_EPILOG = '''
+HELP_EPILOG = """
 
 The purpose of this script is to check file delimiter count mismatches (header vs. detail)
 
 Output file (if invalid) contains header and identified invalid records
 
-'''
+"""
 
-class BadRecord:
-    """Defines bad record with incorrect number of delimiters"""
-    def __init__(self, key, value):  # class constructor
-        self.key = key               # contains field count
-        self.value = value           # contains record
+# Configure logging to emit to STDOUT by default
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s.%(msecs)03d  %(levelname)-10s%(funcName)-22s%(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
-    def __iter__(self):              # allows object to iterate within loop
-        yield self
 
-class ParseDelimitedFile():
+class ParseDelimitedFile:
     r"""
-        A class that parses passed filename by delimiter to verify header record delimiter counts
-        match each detail record
-        
-        :Example:
-        >>> pdf = ParseDelimitedFile(',', r'C:\myfile.csv')
+    A class that parses passed filename by delimiter to verify header record delimiter counts
+    match each detail record
+
+    :Example:
+    >>> pdf = ParseDelimitedFile(',', r'C:\myfile.csv')
+
+    Parameters:
+        delimiter (str): The character used to separate fields in the file.
+        filename (str): The path to the file to be checked.
     """
-    ERROR_DELIMITER_FILE_SUFFIX = '.ERROR_DELIMITER'
-    
-    def __init__(self, delimiter, filename, verbose) -> None:
-        self.delimiter   = delimiter
-        self.filename    = filename
-        self.verbose     = verbose
-        self.badrecords  = []
-    
-    def parse(self) -> bool:
+
+    ERROR_DELIMITER_FILE_SUFFIX = ".ERROR_DELIMITER"
+
+    def __init__(self, delimiter, filename) -> None:
+        self.delimiter = delimiter
+        self.filename = filename
+        self.badrecords = dict()
+        self.logger = logging.getLogger(__name__)
+        self.logger.info(f"Delimiter File Checker Initialized")
+        self.logger.info(f"- Delimiter: {self.delimiter}")
+        self.logger.info(f"-Filename : {self.filename}")
+        self.logger.info("")
+
+    def parse(self) -> None:
         """
         Reads passed delimiter and compares delimiter count of header record (first record) to each detail record
-        
-        :return: Returns True if valid run else raises ValueError
-        :rtype: bool
         """
-        FILESUFFIX = '_' + datetime.now().strftime("%Y_%m_%d_%H_%M_%S") + self.ERROR_DELIMITER_FILE_SUFFIX
+        FILESUFFIX = (
+            "_"
+            + datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+            + self.ERROR_DELIMITER_FILE_SUFFIX
+        )
+
         dict_tally = {}
-        headerDelimiterCount = 0
-        record_count = 0
-        for record in self.read_delimited_record(self.filename):
-            record_count += 1
+        header_delimiter_count = 0
+        for record_count, record in self.read_delimited_record(self.filename):
             if record_count == 1:
-                headerDelimiterCount = record.count(self.delimiter)                 # get header delimiter count
-                self.save_bad_record(record, record_count, headerDelimiterCount)    # append record#, fieldcount, header record to BadRecord list
+                header_delimiter_count = record.count(self.delimiter)
+                self.save_bad_record(record, record_count, header_delimiter_count)
                 continue
 
-            if len(record.strip()) > 0:                             # if any detail data exists
-                detailDelimiterCount = record.count(self.delimiter) # get detail delimiter count
-                if headerDelimiterCount != detailDelimiterCount:    # if header count <> detail, save record
-                    self.save_bad_record(record, record_count, detailDelimiterCount)
+            detail_delimiter_count = record.count(self.delimiter)
+            if header_delimiter_count != detail_delimiter_count:
+                self.save_bad_record(record, record_count, detail_delimiter_count)
 
-                # capture delimiter length statistics                    
-                try:
-                    dict_tally[detailDelimiterCount] += 1
-                except KeyError:
-                    dict_tally[detailDelimiterCount] = 1
+            # capture delimiter length statistics
+            try:
+                dict_tally[detail_delimiter_count] += 1
+            except KeyError:
+                dict_tally[detail_delimiter_count] = 1
 
-        summary = '|'.join([f"{k}: {dict_tally[k]}" for k in dict_tally])
-        if (total_bad_records := len(self.badrecords) - 1):     # write output file to include filename, delimiter, expected fields and all bad records record#, fieldcount, record (including header)
-            message = 'filename :' + self.filename + '\ndelimiter:' + self.delimiter + '\nfields   :' + '{:0>.4f}'.format(headerDelimiterCount / 10000)[-4:] + '\n\nDelimiter Count Summary:\n' + '\n'.join(summary.split('|')) + '\n\nDelimiter Header and Bad Record Detail:\n' + '\n'.join(bad.key + ':' + ''.join(bad.value) for bad in self.badrecords)
-            with open(self.filename + FILESUFFIX, 'w') as badfile:
-                badfile.write(message)
+        if total_bad_records := len(self.badrecords) - 1:
+            self.logger.info("Record Counts:")
+            self.logger.info(f"Bad  : {total_bad_records}")
+            self.logger.info(f"Total: {record_count}")
+            self.logger.info("")
+            self.logger.info("Delimiter Counts: (#delimiters: records)")
+            self.logger.info("- " + str(header_delimiter_count) + ": 1 (header)")
+            for k in dict_tally:
+                self.logger.info(f"- {k}: {dict_tally[k]}")
 
-            if self.verbose:
-                print('- CSV Status: BAD')
-                print('  + Delimiter Summary Record Count: (total delimiters: records)')
-                print('    * ' + str(headerDelimiterCount) + ': 1 (header)')
-                print('    * ' + '\n    * '.join(summary.split('|')))
-            
-            # Format and raise error condition   
-            message = f"File {self.filename} has {total_bad_records} badly delimited record{'s' if total_bad_records > 1 else ''}\n- See file {self.filename + FILESUFFIX} for details"
-            if self.verbose: print(f"{message}\n")
-            raise ValueError(message)
-        else: 
-            if self.verbose: print('- CSV Status: GOOD')
-        
-        return True
+            self.logger.info("")
+            self.logger.info(f"Details: {self.filename + FILESUFFIX}")
+            self.logger.info("")
+            self.logger.info("Status: BAD")
 
-    def read_delimited_record(self, filename: str) -> Iterable[str]:
+            message = []
+            message.append(f"filename : {self.filename}")
+            message.append(f"delimiter: {self.delimiter}")
+            message.append(f"fields   : {(f'{header_delimiter_count/10000:.4f}')[-4:]}")
+            message.append("\n\nDelimiter Count Summary:\n")
+            message.append("\n".join([f"{k}: {dict_tally[k]}" for k in dict_tally]))
+            message.append("\n\nDelimiter Header and Bad Record Detail:\n")
+            for key in sorted(self.badrecords.keys()):
+                message.append(f"{key}: {self.badrecords[key]}\n")
+
+            # write output file to include filename, delimiter, expected fields and all bad records record#, fieldcount, record (including header)
+            with open(self.filename + FILESUFFIX, "w", encoding="utf-8") as badfile:
+                badfile.write("".join(message))
+            sys.exit(1)
+
+        self.logger.info("Status: GOOD")
+        sys.exit(0)
+
+    def read_delimited_record(self, filename: str) -> Iterable[tuple[int, str]]:
         """
-        Reads each record from passed filename using a generator pattern
-        
-        :returns: record (generator object)
-        :rtype: str
+        Reads each record's count and data from passed filename using a generator pattern
+        Args:
+            filename: The path to the file to be read.
+
+        Yields:
+            record (generator object)
+            record count (int)
+            record data (str)
         """
+        ctr = 0
         try:
-            with open(filename, "r", encoding='utf-8') as csvfile:  # open filename with file handle
-                ctr = 0
+            with open(
+                filename, "r", encoding="utf-8"
+            ) as csvfile:  # open filename with file handle
                 for record in csv.reader(csvfile, delimiter=self.delimiter):
-                    yield self.delimiter.join(record)
-                    
+                    ctr += 1
+                    if len(str(record).strip()) > 0:
+                        yield ctr, self.delimiter.join(record)
+
         except UnicodeDecodeError as err:
             message = f"\nRecord: {ctr}\nError: {err}"
-            print(message)
+            self.logger.error(message)
             raise err
-            
-    def save_bad_record(self, record: str, record_count: int, delimiter_count: int) -> None:
-        """append record#, fieldcount, header record to BadRecord list"""
-        self.badrecords.append(BadRecord('{:0>14.4f}'.format(record_count + delimiter_count/10000), record))
 
-def get_args():
-    """
-    Reads, validates, and returns command line arguments
-    
-    :Example:
-    >>> parser = argparse.ArgumentParser()
-    >>> parser.add_argument('--foo')
-    _StoreAction(option_strings=['--foo'], dest='foo', nargs=None, const=None, default=None, type=None, choices=None, required=False, help=None, metavar=None)
-    >>> args = parser.parse_args(['--foo', 'BAR'])
-    >>> vars(args)
-    {'foo': 'BAR'}
-    >>> args.foo
-    'BAR'
-    
-    :return: parsed arguments
-    :rtype: object subclass with a readable string representation
-    """
-    
+    def save_bad_record(
+        self, record: str, record_count: int, delimiter_count: int
+    ) -> None:
+        self.badrecords[f"{record_count + delimiter_count / 10000:0>14.4f}"] = record
+
+
+DEBUG = False
+if __name__ == "__main__":
     parser = ap.ArgumentParser(
-        description='Check file delimiter counts to verify detail vs. header',
+        description="Check file delimiter counts to verify detail vs. header",
         epilog=HELP_EPILOG,
         formatter_class=ap.RawDescriptionHelpFormatter,
     )
-    parser.add_argument('delimiter', type=str, default=",", help='Input file CSV delimiter')
-    parser.add_argument('filename', type=str, help='Input filename')
-    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode: True = print process messages, False(default) = omit process messages')
-    return parser.parse_args()
+    parser.add_argument(
+        "delimiter", type=str, default=",", help="Input file CSV delimiter"
+    )
+    parser.add_argument("filename", type=str, help="Input filename")
 
-DEBUG = False
-if __name__ == '__main__':
     if DEBUG:
-        delimiter = ','
-        filename = r'.\data\badfile.csv'
-        verbose = True
+        args = parser.parse_args(
+            args=[
+                ",",
+                r".\data\badfile.csv",
+            ]
+        )
     else:
-        args = get_args()
-        delimiter = args.delimiter
-        filename = args.filename
-        verbose = args.verbose
-        if args.verbose:
-            print(f'\ndelimitedfilechecker:\n- delimiter: {args.delimiter}\n- filename : {args.filename}\n- verbose  : {args.verbose}')
+        args = parser.parse_args()
 
-    pdf = ParseDelimitedFile(delimiter, filename, verbose)
+    delimiter = args.delimiter
+    filename = args.filename
+    pdf = ParseDelimitedFile(delimiter, filename)
     pdf.parse()
--- test_delimitedfilechecker.py
+++ test_delimitedfilechecker.py
@@ -2,37 +2,96 @@ import delimitedfilechecker as dfc1
 from os import path
 import sys
 import unittest
+import io
+import builtins as _builtins
+from unittest.mock import patch
+
+# preserve original open for fallthrough in mocks
+_original_open = _builtins.open
+
+
+class _StdoutWriter:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc, tb):
+        return False
+
+    def write(self, s):
+        sys.stdout.write(s)
+
+    def flush(self):
+        sys.stdout.flush()
+
+
+def identify(func):
+    def wrapper(*args, **kwargs):
+        if TestDelimitedFileChecker.VERBOSE:
+            print(f"\nTEST: {func.__name__}")
+        return func(*args, **kwargs)
+
+    return wrapper
+
 
 class TestDelimitedFileChecker(unittest.TestCase):
-    VERBOSE = False
-    def identify(func):
-        def wrapper(*args, **kwargs):
-            if TestDelimitedFileChecker.VERBOSE: print(f"\nTEST: {func.__name__}")
-            return func(*args, **kwargs)
-        return wrapper
-    
+    VERBOSE = True
+
     def setUp(self):
-        self.delimiter = "|"
-        self.directory   = path.dirname(path.realpath(sys.argv[0]))+'/data'
-        self.goodfile    = "goodfile.csv"
-        self.badfile     = "badfile.csv"
-        
+        self.delimiter = ","
+        self.directory = path.dirname(path.realpath(sys.argv[0])) + "/data"
+        self.goodfile = "goodfile.csv"
+        self.badfile = "badfile.csv"
+
     @identify
     def test_good_file(self):
-        self.filename = path.join(self.directory, self.goodfile)
-        pdf = dfc1.ParseDelimitedFile(self.delimiter, self.filename, self.VERBOSE)
-        self.assertTrue(pdf.parse())
+        # Provide CSV content where header and detail delimiter counts match
+        good_content = "col1,col2\nval1,val2\nval3,val4\n"
+
+        def open_side_effect(file, mode="r", encoding=None, *args, **kwargs):
+            fname = str(file)
+            # read the mocked good file
+            if fname.endswith(self.goodfile) and "r" in mode:
+                return io.StringIO(good_content)
+            # reroute any ERROR_DELIMITER write output to stdout
+            if "w" in mode and fname.endswith(
+                dfc1.ParseDelimitedFile.ERROR_DELIMITER_FILE_SUFFIX
+            ):
+                return _StdoutWriter()
+            return _original_open(file, mode, encoding=encoding, *args, **kwargs)
+
+        with patch("builtins.open", side_effect=open_side_effect):
+            self.filename = path.join(self.directory, self.goodfile)
+            pdf = dfc1.ParseDelimitedFile(self.delimiter, self.filename)
+            with self.assertRaises(SystemExit) as cm:
+                pdf.parse()
+
+        self.assertEqual(cm.exception.code, 0)
 
     @identify
     def test_bad_file(self):
-        self.filename = path.join(self.directory, self.badfile)
-        pdf = dfc1.ParseDelimitedFile(self.delimiter, self.filename, self.VERBOSE)
-        self.assertRaises(ValueError)
+        # Provide CSV content where a detail row has different delimiter count
+        bad_content = "col1,col2\nval1,val2\nonlyonefield\n"
+
+        def open_side_effect(file, mode="r", encoding=None, *args, **kwargs):
+            fname = str(file)
+            # read the mocked bad file
+            if fname.endswith(self.badfile) and "r" in mode:
+                return io.StringIO(bad_content)
+            # reroute any ERROR_DELIMITER write output to stdout
+            if "w" in mode and fname.endswith(
+                dfc1.ParseDelimitedFile.ERROR_DELIMITER_FILE_SUFFIX
+            ):
+                return _StdoutWriter()
+            return _original_open(file, mode, encoding=encoding, *args, **kwargs)
+
+        with patch("builtins.open", side_effect=open_side_effect):
+            filename = path.join(self.directory, self.badfile)
+            pdf = dfc1.ParseDelimitedFile(self.delimiter, filename)
+            with self.assertRaises(SystemExit) as cm:
+                pdf.parse()
+
+        self.assertEqual(cm.exception.code, 1)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == '-v':   
-        TestDelimitedFileChecker.VERBOSE = True
-    else:
-        TestDelimitedFileChecker.VERBOSE = False
-        
     unittest.main()
